f555eb4f7d3b52db56d260253dc7e76f394e9fd5
BerinGudic <berin.gudic@gmail.com>
2022-03-30 13:17:08 +0200

    Added index.js file

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+    // Ovo je totalno nebitno!
+    /* Ovo je jednako nebitno
+    u vise
+    redova */
+    // Only change code above this line
+
+}
+
+console.log(main());
+
+module.exports = main;
